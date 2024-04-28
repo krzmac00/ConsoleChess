f0f9ca45013c22354afc861a6457643c6f3cd90e
230456 Macherzyński Krzysztof <230456@edu.p.lodz.pl>
2024-04-28 14:14:47 +0200

    delete cmake-buid-debug

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/e/CLionProjects/angielskieSzachy
-# Build directory: /mnt/e/CLionProjects/angielskieSzachy/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("program")
-subdirs("library")
--- cmake-build-debug/library/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/e/CLionProjects/angielskieSzachy/library
-# Build directory: /mnt/e/CLionProjects/angielskieSzachy/cmake-build-debug/library
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(LibraryTest1 "LibraryTest" "--report_level=detailed" "--log_level=all" "--color_output=yes")
-set_tests_properties(LibraryTest1 PROPERTIES  _BACKTRACE_TRIPLES "/mnt/e/CLionProjects/angielskieSzachy/library/CMakeLists.txt;67;add_test;/mnt/e/CLionProjects/angielskieSzachy/library/CMakeLists.txt;0;")
